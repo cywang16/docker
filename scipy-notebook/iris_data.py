e29c1e6d4d3ce1ee43b752583b2389f673fa075c
Chun-Yen Wang <cywang@CentOS1611.voicebox>
2017-06-19 17:12:44 -0700

    added comment in iris_data.py

--- scipy-notebook/iris_data.py
+++ scipy-notebook/iris_data.py
@@ -1,3 +1,8 @@
+'''
+Your First Machine Learning Project in Python Step-By-Step
+----------------------------------------------------------
+http://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+'''
 # Load libraries
 import pandas
 from pandas.tools.plotting import scatter_matrix
