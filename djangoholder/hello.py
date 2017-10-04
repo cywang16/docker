b5c2b0ef0bb275cfdd2cf0b111039a59ec2e8816
cywang <cywang@centos1611va01-localdomain.guest.corp.microsoft.com>
2017-10-03 17:45:31 -0700

    fix flask script

--- djangoholder/hello.py
+++ djangoholder/hello.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/flask")
 def hello():
     return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug = True, host = '0.0.0.0')
