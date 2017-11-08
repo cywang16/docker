7162d87e8ef65f9ad92c6139ca3e52ab57b725c5
Chun-Yen Wang <cywang16@gmail.com>
2017-11-08 11:43:30 -0800

    organized the tutorial

--- myopencv/tutorials/GuiFeaturesInOpenCV/cv2drawtest.py
+++ myopencv/tutorials/GuiFeaturesInOpenCV/cv2drawtest.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
+import os
+
+messi5jpg = os.path.dirname(os.path.realpath(__file__)) + '/messi5.jpg'
 
 # Load an color image in grayscale
-img = cv2.imread('messi5.jpg',0)
+img = cv2.imread(messi5jpg, 0)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
