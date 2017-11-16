9f26b288a4cb7f72f48c9c65740becfe34339887
cywang <cywang@centos1611va01-localdomain.guest.corp.microsoft.com>
2017-11-15 17:16:45 -0800

    polls tutorial 20171115

--- djangoholder/tutorials/mysite/mysite/settings.py
+++ djangoholder/tutorials/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [u'10.122.104.134']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
