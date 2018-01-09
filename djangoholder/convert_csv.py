cb2002f89b31d7bb2cb5a9700adbe354fdee6969
cywang <cywang@centos1611va01-localdomain.guest.corp.microsoft.com>
2018-01-09 15:59:57 -0800

    remove Gemfile.lock and add convert_csv.py

---
+++ djangoholder/convert_csv.py
@@ -0,0 +1,57 @@
+
+import os, sys
+import pandas as pd
+
+import pytz
+from datetime import datetime
+
+dfx = pd.read_excel(open('CourseFactorTable.xlsx','rb'), sheetname='Sheet1')
+g_tz_pst = pytz.timezone('America/Los_Angeles')
+
+def compact_datetime_String():
+    return datetime.now(g_tz_pst).strftime("%Y%m%d-%H%M-%Z")
+
+def fix_csv(filename):
+    classdata = pd.read_csv(filename)
+    del classdata['textBox35']
+    del classdata['textBox5']
+    del classdata['textBox37']
+    del classdata['textBox38']
+    cols = classdata.columns.tolist()
+    cols = cols[-3:-2] + cols[:-3] + cols[-2:]
+    cols = cols[0:7] + cols[-2:-1] + cols[7:-2] + cols[-1:]
+    classdataFixed = classdata[cols]
+    classdataFixed.columns = ['Class Code',
+                              'Room',
+                              'Min',
+                              'Max',
+                              'Pending',
+                              'Enrolled',
+                              'Waitlisted',
+                              'Open',
+                              'Start Date',
+                              'End Date',
+                              'Start Time',
+                              'End Time',
+                              'Days',
+                              'Adjudicator',
+                              'Status']
+    classdataFixed['Factor'] = 0
+    classdataFixed['Time'] = 0
+    for i, row in classdataFixed.iterrows():
+        r = dfx[(dfx['CourseCode'] == row['Class Code']) |
+                (dfx['CourseCode'] == row['Class Code'][:-2])]
+        if len(r.index) == 1:
+            factor = r.iloc[0]['ScheduleFactor']
+            classdataFixed.set_value(i, 'Factor', factor)
+            classdataFixed.set_value(i, 'Time', factor * (row['Enrolled'] + row['Pending']))
+    cols = classdataFixed.columns.tolist()
+    cols = cols[:2] + cols[-2:] + cols[2:-2]
+    classdataFixed = classdataFixed[cols]
+    filenamefixed = 'CourseSummary-{}.csv'.format(compact_datetime_String())
+    classdataFixed.to_csv(filenamefixed, index = False)
+    return filenamefixed
+
+
+if __name__ == "__main__":
+    fix_csv(sys.argv[1])
