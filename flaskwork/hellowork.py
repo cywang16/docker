f47c83fdfba04b6ae1736058e8ff88b75d799ba6
Chun-Yen Wang <cywang16@gmail.com>
2018-06-06 10:11:08 -0700

    tried string parameter

--- flaskwork/hellowork.py
+++ flaskwork/hellowork.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World at work!'
+@app.route('/<user>')
+def hello_world(user):
+    return 'Hello, {} at work!'.format(user)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
